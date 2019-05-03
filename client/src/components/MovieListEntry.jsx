81120ad4f703180674580226ca1d4c27bbedef98
Cokane415 <catherineaokane@gmail.com>
2019-05-03 16:13:04 -0700

    Implement movie list entry component.

--- client/src/components/MovieListEntry.jsx
+++ client/src/components/MovieListEntry.jsx
@@ -8,13 +8,4 @@ var MovieListEntry = (props) => (
     </tr>
 );
 
-//PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
-//npm prop-types
-// MovieListEntry.propTypes = {
-//   movie: React.PropTypes.object.isRequired
-// };
-
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
 export default MovieListEntry;
