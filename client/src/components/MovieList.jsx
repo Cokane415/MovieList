253fc5b9fddc8de6aad755c8a7be3d9903dbe3e9
Cokane415 <catherineaokane@gmail.com>
2019-05-03 16:12:10 -0700

    Implement movie list component.

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -13,13 +13,5 @@ var MovieList = (props) => (
   </table>
 );
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
-// MovieList.propTypes = 
-//   movies: React.PropTypes.object.isRequired
-// };
-
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
 export default MovieList;
 
